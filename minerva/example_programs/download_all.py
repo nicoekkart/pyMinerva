c8557ac25c552d09598fc5949e7f30d5941e1e45
nekkart <Nico.Ekkart@UGent.be>
2016-03-03 17:37:31 +0100

    made the downloader a bit more pythonic, to allow for importing from other files.

--- minerva/example_programs/download_all.py
+++ minerva/example_programs/download_all.py
@@ -4,27 +4,15 @@
 
         with <course_needle> being the search string for your course, and (<folder>) being the path on your computer
         where you want the files to be downloaded.
-        """
+"""
 from minerva_core import Minerva
 import sys
 import os
 import secrets
 
-def download_course(searchString=""):
+
+def download_course(needle_course):
     # Search string for the course
-    needle_course = searchString
-
-    while needle_course is "":
-        needle_course = input("type a keyword for the course you want to download:")
-        if needle_course == "":
-            needle_course = None
-
-        directory = input("Select a directory where to download the course (Leave blank for current directory):")
-        if directory is not "":
-            if not os.path.isdir(directory):
-                os.makedirs(directory)
-            # Change directory
-            os.chdir(directory)
 
     with Minerva(secrets.username, secrets.password) as minerva:
         # Course object
@@ -33,4 +21,24 @@ def download_course(searchString=""):
         minerva.open_tool('docu')
         minerva.get_all_documents()
 
-download_course()
+
+def get_input():
+    needle_course = ""
+    while needle_course is "":
+        needle_course = input(
+            "type a keyword for the course you want to download: ")
+        if needle_course == "":
+            needle_course = None
+
+        directory = input(
+            "Select a directory where to download the course "
+            "(Leave blank for current directory): ")
+        if directory is not "":
+            if not os.path.isdir(directory):
+                os.makedirs(directory)
+            # Change directory
+            os.chdir(directory)
+    return needle_course
+
+if __name__ == '__main__':
+    download_course(get_input())
